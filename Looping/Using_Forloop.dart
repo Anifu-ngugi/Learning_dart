3697bb36c584f85d6cabbc5cc94838c8d83843f3
Anifu-ngugi <anifungugi1@gmail.com>
2024-09-27 12:11:59 +0300

    listing a list using a for loop

---
+++ Looping/Using_Forloop.dart
@@ -0,0 +1,8 @@
+void main(){
+  //using for loop to display players from a list 
+  List <String> players = ["Messi", "Ronaldo","Neymar","Mbampee","Salah"];
+
+  for(String teamPlayers in players){
+    print(teamPlayers);
+  }
+}
