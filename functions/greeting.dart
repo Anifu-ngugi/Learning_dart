2a1307e57d152d44b29cc5ba337a5e550f5aa308
Anifu-ngugi <anifungugi1@gmail.com>
2024-09-27 16:38:46 +0300

    Function greetings

---
+++ functions/greeting.dart
@@ -0,0 +1,10 @@
+///welcome to function declaration, format returType functionName the parameters
+void greetings(){
+  print("welcome to the first function");
+}
+
+// declaring the main function 
+void main(){
+  // calling function greetings
+  greetings();
+}
