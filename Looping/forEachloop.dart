42d34ebec78a4adf6abcba092b301346301c8ad3
Anifu-ngugi <anifungugi1@gmail.com>
2024-09-27 11:47:23 +0300

    using forEach to display a  list of contries

---
+++ Looping/forEachloop.dart
@@ -0,0 +1,6 @@
+void main(){
+  //format collection.forEach(void f(value))
+  List <String> countries = ["Kenya","Somali","Malawi","Tanzania"];
+  // collection is the list the void with the 
+  countries.forEach( (name)=>print(name)); 
+}
