431ec596e14440f1f4af3df85b6a6ff1d2c167b5
Anifu-ngugi <anifungugi1@gmail.com>
2024-09-29 16:54:03 +0300

    adding two numbers

---
+++ functions/sum.dart
@@ -0,0 +1,8 @@
+  //function declaration 
+  void sumOfDigits(int num1, int num2){
+    int sum = num1 + num2;
+    print("the sum of the numbers is:$sum");
+  }
+  void main(){
+    sumOfDigits(10,20);
+  }
