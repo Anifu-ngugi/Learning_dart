7588a414a17dcc20feecb2a1ddd52d52c267a9ed
Anifu-ngugi <anifungugi1@gmail.com>
2024-09-30 21:00:15 +0300

    How add content on a file

--- fileHandling.dart/addContentOnFile.dart
+++ fileHandling.dart/addContentOnFile.dart
@@ -2,6 +2,6 @@ import 'dart:io';
 
 void main(){
   File file=File('test.txt');
-  file.writeAsString("\n we need need leaders who are well versed in the area of appointment \n Thank you!",mode: FileMode.append);
+  file.writeAsString("\n we need need leaders who are well versed in the area of appointment \n Thank you",mode: FileMode.append);
   print("sucessfully updated");
 }
